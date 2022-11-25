cd9280532b88ceb375df361c3f7a0b8d428f5817
abdalla3yash <abdo3yash@gmail.com>
2022-11-25 19:13:46 +0400

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,54 @@
+// import 'package:word/models/error_model.dart';
+// import 'package:word/repository/auth_repository.dart';
+// import 'package:word/router.dart';
+// import 'package:word/screens/document_screen.dart';
+// import 'package:word/screens/home_screen.dart';
+// import 'package:word/screens/login_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:routemaster/routemaster.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:html_editor_enhanced/html_editor.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:word/word_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(HtmlEditorExampleApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HtmlEditorExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Word',
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      home: const WordScreen(),
     );
   }
 }
---
+++ lib/word_screen.dart
@@ -0,0 +1,398 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:html_editor_enhanced/html_editor.dart';
+
+class WordScreen extends StatefulWidget {
+  const WordScreen({super.key});
+
+  @override
+  State<WordScreen> createState() => _WordScreenState();
+}
+
+class _WordScreenState extends State<WordScreen> {
+  String result = '';
+  final HtmlEditorController controller = HtmlEditorController();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (!kIsWeb) {
+          controller.clearFocus();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Document"),
+          elevation: 0,
+          centerTitle: true,
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.refresh),
+                onPressed: () {
+                  if (kIsWeb) {
+                    controller.reloadWeb();
+                  } else {
+                    controller.editorController!.reload();
+                  }
+                })
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            controller.toggleCodeView();
+          },
+          child: const Text(
+            r'<\>',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              HtmlEditor(
+                controller: controller,
+                htmlEditorOptions: const HtmlEditorOptions(
+                  hint: 'Your text here...',
+                  shouldEnsureVisible: true,
+                ),
+                htmlToolbarOptions: HtmlToolbarOptions(
+                  toolbarPosition: ToolbarPosition.belowEditor,
+                  toolbarType: ToolbarType.nativeGrid,
+                  onButtonPressed:
+                      (ButtonType type, bool? status, Function? updateStatus) {
+                    return true;
+                  },
+                  onDropdownChanged: (DropdownType type, dynamic changed,
+                      Function(dynamic)? updateSelectedItem) {
+                    return true;
+                  },
+                  mediaLinkInsertInterceptor:
+                      (String url, InsertFileType type) {
+                    return true;
+                  },
+                  mediaUploadInterceptor:
+                      (PlatformFile file, InsertFileType type) async {
+                    return true;
+                  },
+                ),
+                otherOptions: const OtherOptions(height: 550),
+                callbacks: Callbacks(
+                    onBeforeCommand: (String? currentHtml) {},
+                    onChangeContent: (String? changed) {},
+                    onChangeCodeview: (String? changed) {},
+                    onChangeSelection: (EditorSettings settings) {},
+                    onDialogShown: () {},
+                    onEnter: () {},
+                    onFocus: () {},
+                    onBlur: () {},
+                    onBlurCodeview: () {},
+                    onInit: () {},
+                    onImageUploadError: (FileUpload? file, String? base64Str,
+                        UploadError error) {
+                      if (file != null) {}
+                    },
+                    onKeyDown: (int? keyCode) {},
+                    onKeyUp: (int? keyCode) {},
+                    onMouseDown: () {},
+                    onMouseUp: () {},
+                    onNavigationRequestMobile: (String url) {
+                      return NavigationActionPolicy.ALLOW;
+                    },
+                    onPaste: () {},
+                    onScroll: () {}),
+                plugins: [
+                  SummernoteAtMention(
+                      getSuggestionsMobile: (String value) {
+                        var mentions = <String>['test1', 'test2', 'test3'];
+                        return mentions
+                            .where((element) => element.contains(value))
+                            .toList();
+                      },
+                      mentionsWeb: ['test1', 'test2', 'test3'],
+                      onSelect: (String value) {}),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.blueGrey),
+                      onPressed: () {
+                        controller.undo();
+                      },
+                      child: const Text('Undo',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.blueGrey),
+                      onPressed: () {
+                        controller.clear();
+                      },
+                      child: const Text('Reset',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary),
+                      onPressed: () async {
+                        var txt = await controller.getText();
+                        if (txt.contains('src=\"data:')) {
+                          txt =
+                              '<text removed due to base-64 data, displaying the text could cause the app to crash>';
+                        }
+                        setState(() {
+                          result = txt;
+                        });
+                      },
+                      child: const Text(
+                        'Submit',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary),
+                      onPressed: () {
+                        controller.redo();
+                      },
+                      child: const Text(
+                        'Redo',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(result),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.blueGrey),
+                      onPressed: () {
+                        controller.disable();
+                      },
+                      child: const Text('Disable',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary),
+                      onPressed: () async {
+                        controller.enable();
+                      },
+                      child: const Text(
+                        'Enable',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary),
+                      onPressed: () {
+                        controller.insertText('Google');
+                      },
+                      child: const Text('Insert Text',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary),
+                      onPressed: () {
+                        controller.insertHtml(
+                            '''<p style="color: blue">Google in blue</p>''');
+                      },
+                      child: const Text('Insert HTML',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary),
+                      onPressed: () async {
+                        controller.insertLink(
+                            'Google linked', 'https://google.com', true);
+                      },
+                      child: const Text(
+                        'Insert Link',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary),
+                      onPressed: () {
+                        controller.insertNetworkImage(
+                            'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png',
+                            filename: 'Google network image');
+                      },
+                      child: const Text(
+                        'Insert network image',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.blueGrey),
+                      onPressed: () {
+                        controller.addNotification(
+                            'Info notification', NotificationType.info);
+                      },
+                      child: const Text('Info',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.blueGrey),
+                      onPressed: () {
+                        controller.addNotification(
+                            'Warning notification', NotificationType.warning);
+                      },
+                      child: const Text('Warning',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary),
+                      onPressed: () async {
+                        controller.addNotification(
+                            'Success notification', NotificationType.success);
+                      },
+                      child: const Text(
+                        'Success',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary),
+                      onPressed: () {
+                        controller.addNotification(
+                            'Danger notification', NotificationType.danger);
+                      },
+                      child: const Text(
+                        'Danger',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.blueGrey),
+                      onPressed: () {
+                        controller.addNotification('Plaintext notification',
+                            NotificationType.plaintext);
+                      },
+                      child: const Text('Plaintext',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary),
+                      onPressed: () async {
+                        controller.removeNotification();
+                      },
+                      child: const Text(
+                        'Remove',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
